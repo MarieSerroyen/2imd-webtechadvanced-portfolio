c0989597b183318898425085c47a6480ae504e81
MarieSerroyen <marie.serroyen@gmail.com>
2022-03-15 16:24:52 +0100

    feat: function getLocation works

--- lab4/lab4WeatherApp/classes/App.js
+++ lab4/lab4WeatherApp/classes/App.js
@@ -1,3 +1,21 @@
 export default class App {
-    
+    constructor() {
+        this.getLocation();
+        this.lat = 0;
+        this.lng = 0;
+    }
+
+    getLocation() {
+        console.log("Getting the location");
+        navigator.geolocation.getCurrentPosition(this.gettingLocation.bind(this), this.notGettingLocation.bind(this));
+    }
+
+    gettingLocation() {
+        console.log(this.lat);
+    }
+
+    notGettingLocation(err) {
+        console.log(err);
+    }
+
 }
