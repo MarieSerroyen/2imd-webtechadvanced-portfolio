6dad9a23a846882a76b7663fef438da0ed0863ee
MarieSerroyen <marie.serroyen@gmail.com>
2022-03-15 16:47:10 +0100

    feat: weather API works and gives back temperature and description weather

--- lab4/lab4WeatherApp/classes/App.js
+++ lab4/lab4WeatherApp/classes/App.js
@@ -1,8 +1,9 @@
 export default class App {
-    constructor() {
+    constructor(API_KEY) {
         this.getLocation();
         this.lat = 0;
         this.lng = 0;
+        this.API_KEY = API_KEY;
     }
 
     getLocation() {
@@ -13,7 +14,7 @@ export default class App {
     gettingLocation(location) {
         this.lat = location.coords.latitude;
         this.lng = location.coords.longitude;
-
+        //console.log(location);
         /*console.log(this.lat);
         console.log(this.lng);*/
         this.getWeather();
@@ -24,7 +25,27 @@ export default class App {
     }
 
     getWeather() {
-        console.log("Getting the weather");
+        //console.log("Getting the weather");
+        let url = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${this.lat}&lon=${this.lng}&appid=${this.API_KEY}`;
+        //console.log(url);
+
+        fetch(url).then( (res) => {
+            return res.json(); 
+        }).then( (json) => {
+            console.log(json);
+            this.printWeather(json);
+        }).catch((err) => {
+             console.log(err);
+        }).finally(() => {
+            console.log("finally done");
+        });
+    }
+
+    printWeather(json) {
+        let summary = json.weather[0].description;
+        let temp = Math.round(json.main.temp);
+        document.querySelector("h1").innerHTML = summary;
+        document.querySelector("h2").innerHTML = temp + "°C";
     }
 
 }
--- lab4/lab4WeatherApp/main.js
+++ lab4/lab4WeatherApp/main.js
@@ -1,6 +1,6 @@
 import './style.css';
 
 import App from "./classes/App";
-const app = new App();
+const app = new App("7582e9527e94c39375d30a701e0db911");
 
 
